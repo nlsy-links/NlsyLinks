ffb5490109f29dcfbeed2ac12735df78d3bcf26d
Will Beasley <wibeasley@hotmail.com>
2013-01-28 21:44:22 -0600

    Appending inclusive export pattern to NAMESPACE

--- NlsyLinks/UtilityScripts/Refresh.R
+++ NlsyLinks/UtilityScripts/Refresh.R
@@ -1,7 +1,7 @@
 library(devtools)
 options(device = "windows") #http://support.rstudio.org/help/discussions/problems/80-error-in-function-only-one-rstudio-graphics-device-is-permitted
 
-devtools::document(roclets = c("collate", "namespace","rd") ) #Remember to change this in RStudio too.
+devtools::document(roclets = c("collate", "namespace") ) #,"rd" #Remember to change this in RStudio too.
 devtools::check_doc() #Should return NULL
 #  system("R CMD Rd2pdf --force --output=F:/Projects/RDev/NlsyLinksStaging/NlsyLinks/NlsyLinksDocumentationPeek.pdf F:/Projects/RDev/NlsyLinksStaging/NlsyLinks" )
 
@@ -10,5 +10,5 @@ devtools::run_examples() #This overwrites the NAMESPACE file too
 # devtools::run_examples(, "Ace.Rd")
 test()
 build()
-check()
+devtools::check()
 release()
