6532f6240c138f51de3b6ba8ccf38f46f91635c9
Will Beasley <wibeasley@hotmail.com>
2013-10-30 22:02:11 -0500

    1.207 -another attempt to fix R-Forge's documentation complaint

--- NlsyLinks/UtilityScripts/Refresh.R
+++ NlsyLinks/UtilityScripts/Refresh.R
@@ -1,13 +1,14 @@
 library(devtools)
+# setwd("~nlsylinks/pkg")
 options(device = "windows") #http://support.rstudio.org/help/discussions/problems/80-error-in-function-only-one-rstudio-graphics-device-is-permitted
 
-devtools::document(roclets = c("collate", "namespace","rd") ) # #Remember to change this in RStudio too.
+devtools::build()#args="--resave-data")
+devtools::document(roclets = c("collate", "namespace","rd"), clean=T, reload=T) # #Remember to change this in RStudio too.
 devtools::check_doc() #Should return NULL
 #  system("R CMD Rd2pdf --force --output=./NlsyLinksDocumentationPeek.pdf ." )
 
 devtools::run_examples(); dev.off() #This overwrites the NAMESPACE file too
 # devtools::run_examples(, "Ace.Rd")
-devtools::check_doc()
 devtools::test()
 
 devtools::build()#args="--resave-data")
