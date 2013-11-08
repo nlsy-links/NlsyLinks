1dc23f99df1e5b890f529b25dedae3a15949664f
Will Beasley <wibeasley@hotmail.com>
2013-11-08 10:10:01 -0600

    All the RDAs can be derived from CSVs now
    and don't need to be on Will's dev machine to retrieve the CSVs.
    They're in the NlsyLinksDetermination project: ForDistribution/

--- NlsyLinks/OutsideData/DatasetSources.md
+++ NlsyLinks/OutsideData/DatasetSources.md
@@ -1,6 +1,7 @@
 ### The following files come from the [NlsyLinksDetermination](https://github.com/LiveOak/NlsyLinksDetermination) code written in C#.  Developers looking only at this package won't have access to this code.
-
- * **OutsideData/SubjectDetails79.csv**:
- * **OutsideData/ExtraOutcomes79.csv**:
- * **OutsideData/Links79PairsWithOutcomes.csv**:
- * **OutsideData/Links2011V83.csv**:
+ 
+ * **OutsideData/Links2011Vxx.csv**: This linking file is primary output of the grant.  It has one rowIt can be refreshed by running [AssembleRelatedValues.R](https://github.com/LiveOak/NlsyLinksDetermination/blob/master/ForDistribution/Links/AssembleRelatedValues.R).
+ 
+ * **OutsideData/SubjectDetails79Vxx.csv**: This dataset has one row per subject, and it primarily contains plumbing variables (ie, variables that help assemble larger datasets, like number of siblings and the year of their last completed survey).  It can be refreshed by running [AssembleSubjectDetails.R](https://github.com/LiveOak/NlsyLinksDetermination/blob/master/ForDistribution/SubjectDetails/AssembleSubjectDetails.R).
+ 
+ * **OutsideData/ExtraOutcomes79.csv**: This dataset has one row per subject, and contains primarily outcome measures, intended to be used in examples and demonstrations.  It can be refreshed by running [OutcomesAssembly.R](https://github.com/LiveOak/NlsyLinksDetermination/blob/master/ForDistribution/Outcomes/OutcomesAssembly.R).
--- NlsyLinks/UtilityScripts/IncorporateOutsideDatasets.R
+++ NlsyLinks/UtilityScripts/IncorporateOutsideDatasets.R
@@ -11,39 +11,42 @@ require(RODBC)
 ###############################################################
 directoryDatasetsCsv <- "./OutsideData" #These CSVs are in the repository, but not in the build.
 directoryDatasetsRda <- "./data" #These RDAs are derived from the CSV, and included in the build as compressed binaries.
+algorithmVersion <- 84L
 
-pathInputLinks <- file.path(directoryDatasetsCsv, "Links2011V83.csv")
+pathInputLinks <- file.path(directoryDatasetsCsv, paste0("Links2011V", algorithmVersion, ".csv"))
+pathInputSubjectDetails <- file.path(directoryDatasetsCsv, paste0("SubjectDetailsV", algorithmVersion, ".csv"))
 
+pathOutputExtraOutcomes <- file.path(directoryDatasetsRda, "ExtraOutcomes79.rda")
 pathOutputLinkTrim <- file.path(directoryDatasetsRda, "Links79Pair.rda")
 pathOutputLinkExpanded <- file.path(directoryDatasetsRda, "Links79PairExpanded.rda")
-
-pathOutputSubjectDetailsCsv <- file.path(directoryDatasetsCsv, "SubjectDetails79.csv")
-pathOutputSubjectDetailsRda <- file.path(directoryDatasetsRda, "SubjectDetails79.rda")
+pathOutputSubjectDetails <- file.path(directoryDatasetsRda, "SubjectDetails79.rda")
 
 ###############################################################
 ###  ExtraOutcomes79
 ###############################################################
 ExtraOutcomes79 <- read.csv(file.path(directoryDatasetsCsv, "ExtraOutcomes79.csv"))
-# ExtraOutcomes79$SubjectTag <- round(ExtraOutcomes79$SubjectTag)
+# ExtraOutcomes79$SubjectTag <- as.integer(round(ExtraOutcomes79$SubjectTag))
 
 sapply(ExtraOutcomes79, class)
+save(ExtraOutcomes79, file=pathOutputExtraOutcomes, compress="xz")
+
 ###############################################################
 ###  Links79PairExpanded and Links79Pair 
 ###############################################################
-Links79PairWithoutOutcomes <- read.csv(pathInputLinks, stringsAsFactors=FALSE)
+dsLinks79PairWithoutOutcomes <- read.csv(pathInputLinks, stringsAsFactors=FALSE)
 pairVariablesToDrop <- c("MultipleBirthIfSameSex", "RImplicitSubject", "RImplicitMother")
-Links79PairWithoutOutcomes <- Links79PairWithoutOutcomes[, !(colnames(Links79PairWithoutOutcomes) %in% pairVariablesToDrop)]
+dsLinks79PairWithoutOutcomes <- dsLinks79PairWithoutOutcomes[, !(colnames(dsLinks79PairWithoutOutcomes) %in% pairVariablesToDrop)]
 
-sapply(Links79PairWithoutOutcomes, class)
+sapply(dsLinks79PairWithoutOutcomes, class)
 
 ExtraOutcomes79$SubjectTag <- NlsyLinks::CreateSubjectTag(subjectID=ExtraOutcomes79$SubjectID, generation=ExtraOutcomes79$Generation)
-# colnames(Links79PairWithoutOutcomes)
+# colnames(dsLinks79PairWithoutOutcomes)
 firstNames <- c("Subject1Tag", "Subject2Tag")
-remaining <- setdiff(colnames(Links79PairWithoutOutcomes), firstNames)
+remaining <- setdiff(colnames(dsLinks79PairWithoutOutcomes), firstNames)
 Links79PairExpanded <- NlsyLinks::CreatePairLinksSingleEntered(
   outcomeNames=c("MathStandardized", "HeightZGenderAge"), 
   outcomeDataset=ExtraOutcomes79, 
-  linksPairDataset=Links79PairWithoutOutcomes, 
+  linksPairDataset=dsLinks79PairWithoutOutcomes, 
   linksNames=remaining)
 Links79PairExpanded <- Links79PairExpanded[Links79PairExpanded$Subject1Tag < Links79PairExpanded$Subject2Tag, ]
 
@@ -66,13 +69,7 @@ save(Links79PairExpanded, file=pathOutputLinkExpanded, compress="xz")
 ###############################################################
 ###  SubjectDetails
 ###############################################################
+SubjectDetails79 <- read.csv(pathInputSubjectDetails, stringsAsFactors=TRUE)
 
-
-##TODO:  This section shouldn't pull from a specific database.  This code should be moved to a sister file, like "D:\Projects\BG\Links2011\NlsyLinksDetermination\CodingUtilities\QueryRelatedValues.R"
-
-channel <- RODBC::odbcDriverConnect("driver={SQL Server}; Server=Bee\\Bass; Database=NlsLinks; Uid=NlsyReadWrite; Pwd=nophi")
-SubjectDetails79 <- sqlQuery(channel, "SELECT * FROM NlsLinks.dbo.vewSubjectDetails79")
-odbcClose(channel)
-summary(SubjectDetails79)
-save(SubjectDetails79, file=pathOutputSubjectDetailsRda, compress="xz")
-# saveRDS(dsSubjectDetails, file=file.path(directoryDatasetsRda, "SubjectDetails79.rds"), compress="xz")
+sapply(SubjectDetails79, class)
+save(SubjectDetails79, file=pathOutputSubjectDetails, compress="xz")
