e5000cface0ed1b535abd7adec2b391fbfac28f1
Will Beasley <wibeasley@hotmail.com>
2015-09-28 11:08:51 -0500

    Tidy code. Switch to dplyr.

--- inst/reports/ace-subgroups/ace-subgroups.R
+++ inst/reports/ace-subgroups/ace-subgroups.R
@@ -3,12 +3,11 @@ rm(list=ls(all=TRUE)) #Clear variables from previous runs.
 # @knitr load_sources ------------------------------------------------------------
 
 # @knitr load_packages -----------------------------------------------------------
-library(NlsyLinks)
 library(magrittr)
 library(knitr)
-library(xtable)
+requireNamespace("NlsyLinks", quietly=T)
+requireNamespace("xtable", quietly=T)
 requireNamespace("dplyr", quietly=T)
-requireNamespace("plyr", quietly=T)
 requireNamespace("scales", quietly=T)
 
 # @knitr define_globals ----------------------------------------------------------
@@ -19,13 +18,10 @@ relationshipPaths <- c(2)
 # relationshipPaths <- c(1, 2, 3, 4, 5)
 
 rVersions <- c("R", "RFull", "RExplicit", "RImplicit",  "RImplicit2004")
-
 dropIfHousematesAreNotSameGeneration <- FALSE
-
 rGroupsToDrop <- c()
-# determinantThreshold <- 1e-5
-
 suppressGroupTables <- TRUE
+# determinantThreshold <- 1e-5
 
 # sql <- paste(
 #   "SELECT 
@@ -68,7 +64,7 @@ dsPair <- dsPair[as.integer(dsPair$RelationshipPath) %in% relationshipPaths, ]
 oName_1 <- paste0(oName, "_S1")
 oName_2 <- paste0(oName, "_S2")
 relationshipPathsString <- paste0("(", paste(relationshipPaths, collapse=","), ")")
-relationshipPathsPretty <- paste0("(", paste(levels(Links79Pair$RelationshipPath)[relationshipPaths], collapse=", "), ")")
+relationshipPathsPretty <- paste0("(", paste(levels(dsPair$RelationshipPath)[relationshipPaths], collapse=", "), ")")
 
 dsOutcomes$RandomFakeOutcome <- rnorm(n=nrow(dsOutcomes))
 dsOutcomes <- dsOutcomes[, c("SubjectTag", oName)]
@@ -78,7 +74,7 @@ if( dropIfHousematesAreNotSameGeneration ) {
 }
 
 #They're called 'dirty' because the final cleaning stage hasn't occurred yet (ie, removing unwanted R groups)
-dsDirty <- CreatePairLinksSingleEntered(
+dsDirty <- NlsyLinks::CreatePairLinksSingleEntered(
   outcomeDataset   = dsOutcomes, 
   linksPairDataset = dsPair, 
   linksNames       = rVersions, 
@@ -96,34 +92,36 @@ dsAce <- data.frame(Version=rVersions, ASq=NA_real_, CSq=NA_real_, ESq=NA_real_,
 for( i in seq_along(rVersions) ) {
   rVersion <-  rVersions[i] # rVersion <- "RFull"
 #  print(rVersion)
-  dsGroupSummary <- RGroupSummary(dsDirty, oName_1, oName_2, rName=rVersion)#, determinantThreshold=determinantThreshold)
+  dsGroupSummary <- NlsyLinks::RGroupSummary(dsDirty, oName_1, oName_2, rName=rVersion)#, determinantThreshold=determinantThreshold)
   dsGroupSummary[dsGroupSummary[, rVersion] %in% rGroupsToDrop, "Included"] <- FALSE
   
   groupDatasets[[(i-1)*2 + 1]] <- dsGroupSummary
        
-  dsClean <- CleanSemAceDataset(dsDirty=dsDirty, dsGroupSummary, oName_1, oName_2, rName=rVersion)
+  dsClean <- NlsyLinks::CleanSemAceDataset(dsDirty=dsDirty, dsGroupSummary, oName_1, oName_2, rName=rVersion)
   
-  ace <- AceLavaanGroup(dsClean)
+  ace <- NlsyLinks::AceLavaanGroup(dsClean)
   dsAce[i, 2:5] <- c(ace@ASquared, ace@CSquared, ace@ESquared, ace@CaseCount)
 }
 # dsAce
 # groupDatasets[[1]]
 
-PrintGroupSummary <- function( dsSummary, title="Group Summary"  ) {
-  colnames(dsSummary)[colnames(dsSummary)=="Included"] <- "Included in SEM"
-  colnames(dsSummary)[colnames(dsSummary)=="PairCount"] <- "$N_{Pairs}$"
-  colnames(dsSummary)[colnames(dsSummary)=="O1Mean"] <- "$\\bar{x}_1$"
-  colnames(dsSummary)[colnames(dsSummary)=="O2Mean"] <- "$\\bar{x}_2$"
-  colnames(dsSummary)[colnames(dsSummary)=="O1Variance"] <- "$s_1^2$"
-  colnames(dsSummary)[colnames(dsSummary)=="O2Variance"] <- "$s_2^2$"
-  colnames(dsSummary)[colnames(dsSummary)=="O1O2Covariance"] <- "$s_{1,2}$"
-  colnames(dsSummary)[colnames(dsSummary)=="Correlation"] <- "$r$"
+PrintGroupSummary <- function( dSummary, title="Group Summary"  ) {
+  dSummary <- dplyr::rename_(dSummary,
+    "Included in SEM" = "Included",
+    "$N_{Pairs}$"     = "PairCount",
+    "$\\bar{x}_1$"    = "O1Mean",
+    "$\\bar{x}_2$"    = "O2Mean",
+    "$s_1^2$"         = "O1Variance",
+    "$s_2^2$"         = "O2Variance",
+    "$s_{1,2}$"       = "O1O2Covariance",
+    "$r$"             = "Correlation"
+  )
   
   digitsFormat <- c(0,3,0,0,2,2,2,2,2,2,1,0) #Include a dummy at the beginning, for the row.names.
-  textTable <- xtable(dsSummary, caption=title, digits=digitsFormat)
-  print(textTable, include.rownames=F, sanitize.text.function = function(x) {x}) # size="large", size="small",
+  textTable <- xtable::xtable(dSummary, caption=title, digits=digitsFormat)
+  xtable:::print.xtable(textTable, include.rownames=F, sanitize.text.function = function(x) {x}) # size="large", size="small",
 }
-# PrintGroupSummary(dsSummary=dsGroupSummary)
+# PrintGroupSummary(dSummary=dsGroupSummary)
 
 for( i in seq_along(rVersions) ) {
   rVersion <- rVersions[i]
@@ -135,16 +133,24 @@ for( i in seq_along(rVersions) ) {
 
 # @knitr evaluate_ace ------------------------------------------------------------
 PrintAces <- function( ) {
-  dsT <- dsAce
-  dsT <- cbind(dsT[, 1], plyr::numcolwise(round)(dsT,digits=2))
-  dsT <- cbind(dsT[, 1], plyr::numcolwise(scales::comma)(dsT))
-  dsT <- t(apply(dsT, 1, function(x) gsub("^0.", ".", x)))
+  dAcePretty <- dsAce
+  dAcePretty <- dplyr::rename_(dAcePretty,
+    "$R$ Variant" = "Version", 
+    "$a^2$"       = "ASq", 
+    "$c^2$"       = "CSq", 
+    "$e^2$"       = "ESq", 
+    "$N$"         = "N"
+  )
   
-  colnames(dsT) <- c("$R$ Variant", "$a^2$", "$c^2$", "$e^2$", "$N$")
+  dAcePretty <- cbind(dAcePretty[, 1], plyr::numcolwise(round)(dAcePretty,digits=2))
+  dAcePretty <- cbind(dAcePretty[, 1], plyr::numcolwise(scales::comma)(dAcePretty))
+
+  dAcePretty <- t(apply(dAcePretty, 1, function(x) gsub("^0.", ".", x)))
+  
   digitsFormat <- 2# c(0,1,3,3,3,3,3,3) #Include a dummy at the beginning, for the row.names.
   alignnment <- "ll|rrrr" #Include an initial dummy for the (suppressed) row names.
-  textTable <- xtable(dsT, caption="Comparison of R Variants (by rows) and of Links Versions (left vs right side).", digits=digitsFormat, align=alignnment)
-  print(textTable, include.rownames=F, size="large", sanitize.text.function = function(x) {x}, floating=T)
+  textTable <- xtable::xtable(dAcePretty, caption="Comparison of R Variants (by rows) and of Links Versions (left vs right side).", digits=digitsFormat, align=alignnment)
+  xtable:::print.xtable(textTable, include.rownames=F, size="large", sanitize.text.function = function(x) {x}, floating=T)
 }
 PrintAces()
 # summary(dsAce)
