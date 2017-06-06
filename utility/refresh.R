2c814ec30cb2aa92699e6473732cb7314ceda408
Will Beasley <wibeasley@hotmail.com>
2017-06-06 00:25:46 -0500

    Update pkgdown output after conversion to markdown
    See #34

--- utility/refresh.R
+++ utility/refresh.R
@@ -5,6 +5,7 @@ options(device = deviceType) #http://support.rstudio.org/help/discussions/proble
 
 devtools::document()
 devtools::check_man() #Should return NULL
+pkgdown::build_site()
 system("R CMD Rd2pdf --no-preview --force --output=./documentation-peek.pdf ." )
 
 devtools::run_examples(); dev.off() #This overwrites the NAMESPACE file too
@@ -12,7 +13,6 @@ devtools::run_examples(); dev.off() #This overwrites the NAMESPACE file too
 test_results_checked <- devtools::test()
 # test_results_not_checked <- testthat::test_dir("./tests/manual/")
 devtools::build_vignettes()
-pkgdown::build_site()
 
 # devtools::build(args="--resave-data --no-build-vignettes")#args="--resave-data")
 
