4a7f20975360b8b3c548660aef1ff8bcd7c28445
Will Beasley <wibeasley@hotmail.com>
2018-06-20 20:26:42 -0500

    rewriting rda conversion
    prep for #47

--- utility/incorporate-outside-datasets.R
+++ utility/incorporate-outside-datasets.R
@@ -3,6 +3,7 @@
 rm(list=ls(all=TRUE))
 if( any(search()=="package:NlsyLinks") ) detach("package:NlsyLinks") #So the lazy-loaded datasets aren't available
 # if( any(.packages(all.available=TRUE) == "NlsyLinks") ) remove.packages("NlsyLinks") #system("R CMD REMOVE NlsyLinks") #This shouldn't be necesary.
+library(magrittr)
 library(RODBC)
 # require(NlsyLinks) #Don't load' the lazy-loaded datasets shouldn't be accessible
 
@@ -13,19 +14,19 @@ directoryDatasetsCsv <- "./outside-data" #These CSVs are in the repository, but 
 directoryDatasetsRda <- "./data" #These RDAs are derived from the CSV, and included in the build as compressed binaries.
 algorithmVersion <- 85L
 
-pathInputLinks <- file.path(directoryDatasetsCsv, paste0("links-2011-v", algorithmVersion, ".csv"))
-pathInputSubjectDetails <- file.path(directoryDatasetsCsv, paste0("subject-details-v", algorithmVersion, ".csv"))
-pathInputSurveyDate <- file.path(directoryDatasetsCsv, paste0("survey-time.csv"))
+pathInputLinks              <- file.path(directoryDatasetsCsv, paste0("links-2011-v"     , algorithmVersion, ".csv"))
+pathInputSubjectDetails     <- file.path(directoryDatasetsCsv, paste0("subject-details-v", algorithmVersion, ".csv"))
+pathInputSurveyDate         <- file.path(directoryDatasetsCsv, paste0("survey-time.csv"))
 
-pathOutputExtraOutcomes <- file.path(directoryDatasetsRda, "ExtraOutcomes79.rda")
-pathOutputLinkTrim <- file.path(directoryDatasetsRda, "Links79Pair.rda")
-pathOutputLinkExpanded <- file.path(directoryDatasetsRda, "Links79PairExpanded.rda")
-pathOutputSubjectDetails <- file.path(directoryDatasetsRda, "SubjectDetails79.rda")
-pathOutputSurveyDate <- file.path(directoryDatasetsRda, "SurveyDate.rda")
+pathOutputExtraOutcomes     <- file.path(directoryDatasetsRda, "ExtraOutcomes79.rda")
+pathOutputLinkTrim          <- file.path(directoryDatasetsRda, "Links79Pair.rda")
+pathOutputLinkExpanded      <- file.path(directoryDatasetsRda, "Links79PairExpanded.rda")
+pathOutputSubjectDetails    <- file.path(directoryDatasetsRda, "SubjectDetails79.rda")
+pathOutputSurveyDate        <- file.path(directoryDatasetsRda, "SurveyDate.rda")
 
 ###############################################################
 ###  ExtraOutcomes79
-ExtraOutcomes79 <- read.csv(file.path(directoryDatasetsCsv, "extra-outcomes-79.csv"))
+ExtraOutcomes79             <- read.csv(file.path(directoryDatasetsCsv, "extra-outcomes-79.csv"))
 # ExtraOutcomes79$SubjectTag <- as.integer(round(ExtraOutcomes79$SubjectTag))
 
 sapply(ExtraOutcomes79, class)
@@ -33,39 +34,42 @@ save(ExtraOutcomes79, file=pathOutputExtraOutcomes, compress="xz")
 
 ###############################################################
 ###  Links79PairExpanded and Links79Pair
-dsLinks79PairWithoutOutcomes <- read.csv(pathInputLinks, stringsAsFactors=FALSE)
+dsLinks79PairWithoutOutcomes <- pathInputLinks %>% 
+  read.csv(stringsAsFactors=FALSE) %>% 
+  dplyr::select(-MultipleBirthIfSameSex, -RImplicitSubject, -RImplicitMother)
 
-pairVariablesToDrop <- c("MultipleBirthIfSameSex", "RImplicitSubject", "RImplicitMother")
-dsLinks79PairWithoutOutcomes <- dsLinks79PairWithoutOutcomes[, !(colnames(dsLinks79PairWithoutOutcomes) %in% pairVariablesToDrop)]
 
-sapply(dsLinks79PairWithoutOutcomes, class)
 
 ExtraOutcomes79$SubjectTag <- NlsyLinks::CreateSubjectTag(subjectID=ExtraOutcomes79$SubjectID, generation=ExtraOutcomes79$Generation)
 # colnames(dsLinks79PairWithoutOutcomes)
-firstNames <- c("SubjectTag_S1", "SubjectTag_S2")
-remaining <- setdiff(colnames(dsLinks79PairWithoutOutcomes), firstNames)
-Links79PairExpanded <- NlsyLinks::CreatePairLinksSingleEntered(
-  outcomeNames=c("MathStandardized", "HeightZGenderAge"),
-  outcomeDataset=ExtraOutcomes79,
-  linksPairDataset=dsLinks79PairWithoutOutcomes,
-  linksNames=remaining)
-Links79PairExpanded <- Links79PairExpanded[Links79PairExpanded$SubjectTag_S1 < Links79PairExpanded$SubjectTag_S2, ]
+remaining           <- setdiff(colnames(dsLinks79PairWithoutOutcomes),  c("SubjectTag_S1", "SubjectTag_S2"))
+relationshipLabels  <- c("Gen1Housemates","Gen2Siblings","Gen2Cousins","ParentChild", "AuntNiece")
+
+Links79PairExpanded <- c("MathStandardized", "HeightZGenderAge") %>% 
+  NlsyLinks::CreatePairLinksSingleEntered(
+    outcomeNames      = .,
+    outcomeDataset    = ExtraOutcomes79,
+    linksPairDataset  = dsLinks79PairWithoutOutcomes,
+    linksNames        = remaining
+  ) %>% 
+  dplyr::filter(SubjectTag_S1 < SubjectTag_S2) %>% 
+  dplyr::mutate(
+    RelationshipPath  = factor(RelationshipPath, levels=seq_along(relationshipLabels), labels=relationshipLabels),
+    IsMz              = factor(IsMz            , levels=c(0, 1, 255), labels=c("No", "Yes", "DoNotKnow")),
+    EverSharedHouse   = as.logical(EverSharedHouse)
+    
+  ) %>% 
+  dplyr::select(-RImplicitDifference) %>% 
+  dplyr::arrange(ExtendedID, SubjectTag_S1, SubjectTag_S2)
+
 
 ### Prepare for rda
-Links79PairExpanded <- Links79PairExpanded[order(Links79PairExpanded$ExtendedID, Links79PairExpanded$SubjectTag_S1, Links79PairExpanded$SubjectTag_S2), ]
-relationshipLabels <- c("Gen1Housemates","Gen2Siblings","Gen2Cousins","ParentChild", "AuntNiece")
 # multipleBirthLabels <- c("No", "Twin", "Triplet", "DoNotKnow")
-
-Links79PairExpanded$RelationshipPath <- factor(Links79PairExpanded$RelationshipPath, levels=seq_along(relationshipLabels), labels=relationshipLabels)
 # Links79PairExpanded$MultipleBirth <- factor(Links79PairExpanded$MultipleBirth, levels=c(0, 2, 3, 255), labels=multipleBirthLabels)
-Links79PairExpanded$IsMz <- factor(Links79PairExpanded$IsMz, levels=c(0, 1, 255), labels=c("No", "Yes", "DoNotKnow"))
-Links79PairExpanded$EverSharedHouse <- as.logical(Links79PairExpanded$EverSharedHouse)
 
-Links79PairExpanded$RImplicitDifference <- NULL #Drop RImplicitDifference
+Links79Pair <- Links79PairExpanded %>% 
+  dplyr::select(ExtendedID, SubjectTag_S1, SubjectTag_S2, R, RelationshipPath)
 
-Links79Pair <- Links79PairExpanded[, c("ExtendedID", "SubjectTag_S1", "SubjectTag_S2", "R", "RelationshipPath")]
-
-sapply(Links79PairExpanded, class)
 save(Links79Pair, file=pathOutputLinkTrim, compress="xz")
 save(Links79PairExpanded, file=pathOutputLinkExpanded, compress="xz")
 
