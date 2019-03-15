b8243b9b464df073f813b187aff61ffe4cd0b9b4
Will Beasley <wibeasley@hotmail.com>
2019-03-14 22:35:35 -0500

    update count

--- tests/testthat/test-datasets.R
+++ tests/testthat/test-datasets.R
@@ -5,8 +5,8 @@ test_that("Links79Pair", {
   expectedColumnCount <- 5
   actualColumnCount <- ncol(Links79Pair)
   expect_equal(actualColumnCount, expectedColumnCount, info="The number of columns should be correct.")
-  
-  expectedRowCount <- 42773 #42714 #11075
+
+  expectedRowCount <- 42836 #42773 #42714 #11075
   actualRowCount <- nrow(Links79Pair)
   expect_equal(actualRowCount, expectedRowCount, info="The number of rows should be correct.")
 })
@@ -15,8 +15,8 @@ test_that("Links79PairExpanded", {
   expectedColumnCount <- 26
   actualColumnCount <- ncol(Links79PairExpanded)
   expect_equal(actualColumnCount, expectedColumnCount, info="The number of columns should be correct.")
-  
-  expectedRowCount <- 42773 #42714 #11075
+
+  expectedRowCount <- 42836 #42773 #42714 #11075
   actualRowCount <- nrow(Links79PairExpanded)
   expect_equal(actualRowCount, expectedRowCount, info="The number of rows should be correct.")
 })
