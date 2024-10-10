63499c814723576c050816d56cc1264fffa00c0d
Mason Garrison <garrissm@wfu.edu>
2024-10-10 11:05:13 -0400

    Update pkgdown.yaml

--- vignettes/articles/compile_joss.R
+++ vignettes/articles/compile_joss.R
@@ -1,9 +1,9 @@
 compile_joss <- function(...) {
   message("Rendering rmarkdown document...")
 
-  rmarkdown::render('E:/Dropbox/Lab/Research/Projects/2023/NlsyLinks/vignettes/articles/paper.Rmd',
-                    encoding = 'UTF-8',
-                    knit_root_dir = '~/R')
+  #rmarkdown::render('E:/Dropbox/Lab/Research/Projects/2023/NlsyLinks/vignettes/articles/paper.Rmd',
+#                    encoding = 'UTF-8',
+#                    knit_root_dir = '~/R')
 
   message("Done rendering!")
 }
